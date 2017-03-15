37cf638822d5db91826eccb07357b96cc68224dd
jampol <rafaellucenamartinez@mbp-de-rafael.home>
2017-03-15 13:19:49 +0100

    Se ha creado archivos para java

---
+++ PruebaGithub/src/pruebagithub/PruebaGithub.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebagithub;
+
+
+
+//ESta es la pagina donde podremos ver nuestros trabajos
+
+/**
+ *
+ * @author rafaellucenamartinez
+ */
+public class PruebaGithub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
